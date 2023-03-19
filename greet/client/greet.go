fc41a532c603cd173257e86ee997e3eb69761020
john <johncardona1992@gmail.com>
2023-03-18 21:01:20 -0500

    first Unary

---
+++ greet/client/greet.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"log"
+	pb "zelator/grpc/greet/proto"
+)
+
+func doGreet(c pb.GreetServiceClient) {
+	log.Println("doGreet was invoked")
+	res, err := c.Greet(context.Background(), &pb.GreetRequest{
+		FirstName: "ZELATOR",
+	})
+
+	if err != nil {
+		log.Fatalf("Could not greet: %v\n", err)
+	}
+
+	log.Printf("Greeting: %s\n", res.Result)
+}
